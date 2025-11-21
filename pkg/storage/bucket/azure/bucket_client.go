fa71be115a254ca3980d9a8aaed1f9726e778c96
Anhui-tqhuang <tianqiu.huang@enterprisedb.com>
2025-11-21 12:35:47 +0800

    auto detect data lake gen 2
    Signed-off-by: Anhui-tqhuang <tianqiu.huang@enterprisedb.com>

--- pkg/storage/bucket/azure/bucket_client.go
+++ pkg/storage/bucket/azure/bucket_client.go
@@ -19,7 +19,7 @@ func NewBucketClient(cfg Config, name string, logger log.Logger, wrapRT func(htt
 	}, nil
 }
 
-func newBucketClient(cfg Config, name string, logger log.Logger, wrapRT func(http.RoundTripper) http.RoundTripper, factory func(log.Logger, azure.Config, string, func(http.RoundTripper) http.RoundTripper) (*azure.Bucket, error)) (objstore.Bucket, error) {
+func newBucketClient(cfg Config, name string, logger log.Logger, wrapRT func(http.RoundTripper) http.RoundTripper, factory func(log.Logger, azure.Config, string, func(http.RoundTripper) http.RoundTripper) (objstore.Bucket, error)) (objstore.Bucket, error) {
 	// Start with default config to make sure that all parameters are set to sensible values, especially
 	// HTTP Config field.
 	bucketConfig := azure.DefaultConfig
